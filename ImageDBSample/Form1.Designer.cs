6eab30d78d5afaabaa984cfa737c7ad82df7e12c
ThansiJamal <thansijamal18@gmail.com>
2020-10-20 09:57:24 -0700

    first commit

---
+++ ImageDBSample/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ImageDBSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picP1 = new System.Windows.Forms.PictureBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picP1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picP1
+            // 
+            this.picP1.Location = new System.Drawing.Point(154, 12);
+            this.picP1.Name = "picP1";
+            this.picP1.Size = new System.Drawing.Size(259, 182);
+            this.picP1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picP1.TabIndex = 0;
+            this.picP1.TabStop = false;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(154, 220);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(115, 23);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(304, 220);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(109, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 256);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnBrowser);
+            this.Controls.Add(this.picP1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picP1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picP1;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ImageDBSample/Form1.cs
@@ -0,0 +1,42 @@
+﻿using ImageFS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageDBSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            picP1.Image = Image.FromFile(openFileDialog1.FileName);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection(@"Server = DESKTOP-1GLFNAF\SQLEXPRESS; Database =ImageDB; trusted_connection = true");
+            connect.Open();
+            SqlCommand cmd = new SqlCommand("ImageStringSave", connect);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ImageString", ImageProcessing.ImageToBase64(picP1.Image, ImageFormat.Png));
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("saved");
+            connect.Close();
+
+        }
+    }
+}
